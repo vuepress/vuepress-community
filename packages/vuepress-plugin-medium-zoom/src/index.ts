e196a0ba2716b287699c91bab488e6ac77356646
meteorlxy <meteor.lxy@foxmail.com>
2019-10-12 15:58:58 +0800

    fix(vuepress-plugin-medium-zoom): add options type

--- packages/vuepress-plugin-medium-zoom/src/index.ts
+++ packages/vuepress-plugin-medium-zoom/src/index.ts
@@ -1,13 +1,20 @@
 import { Plugin } from 'vuepress-types'
+import { ZoomOptions } from 'medium-zoom'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
+
+export interface MediumZoomPluginOptions {
+  selector: string
+  options: ZoomOptions
+  delay: number
+}
 
 const MediumZoomPlugin: Plugin = ({
   selector = '.theme-default-content img',
   options = {},
   delay = 500,
-}) => ({
+}: MediumZoomPluginOptions) => ({
   name: 'vuepress-plugin-medium-zoom',
 
   clientRootMixin: path.resolve(__dirname, 'clientRootMixin.js'),
@@ -15,7 +22,7 @@ const MediumZoomPlugin: Plugin = ({
   define: {
     MZ_SELECTOR: selector,
     MZ_OPTIONS: JSON.stringify(options),
-    MZ_DELAY: delay,
+    MZ_DELAY: delay.toString(),
   },
 })
 
