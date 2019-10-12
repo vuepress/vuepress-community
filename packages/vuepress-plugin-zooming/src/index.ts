01e0c2bcfffe8f0832611df3f154868fa1655611
meteorlxy <meteor.lxy@foxmail.com>
2019-10-12 15:58:48 +0800

    fix(vuepress-plugin-zooming): add options type

--- packages/vuepress-plugin-zooming/src/index.ts
+++ packages/vuepress-plugin-zooming/src/index.ts
@@ -3,11 +3,35 @@ import { Plugin } from 'vuepress-types'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
+export interface ZoomingPluginOptions {
+  selector: string
+  options: {
+    bgColor?: string
+    bgOpacity?: number
+    closeOnWindowResize?: boolean
+    customSize?:
+      | string
+      | {
+          width: number
+          height: number
+        }
+    enableGrab?: boolean
+    preloadImage?: boolean
+    scaleBase?: number
+    scaleExtra?: number
+    scrollThreshold?: number
+    transitionDuration?: number
+    transitionTimingFunction?: string
+    zIndex?: number
+  }
+  delay: number
+}
+
 const ZoomingPlugin: Plugin = ({
   selector = '.theme-default-content img',
   options = {},
   delay = 500,
-}) => ({
+}: ZoomingPluginOptions) => ({
   name: 'vuepress-plugin-zooming',
 
   clientRootMixin: path.resolve(__dirname, 'clientRootMixin.js'),
@@ -15,7 +39,7 @@ const ZoomingPlugin: Plugin = ({
   define: {
     ZOOMING_SELECTOR: selector,
     ZOOMING_OPTIONS: JSON.stringify(options),
-    ZOOMING_DELAY: delay,
+    ZOOMING_DELAY: delay.toString(),
   },
 })
 
