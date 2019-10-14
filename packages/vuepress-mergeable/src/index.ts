6e3f980ae01653534c6fe6c25e72f7be6dc3b161
meteorlxy <meteor.lxy@foxmail.com>
2019-10-14 15:02:43 +0800

    feat(vuepress-mergeable): migrate with types

---
+++ packages/vuepress-mergeable/src/index.ts
@@ -0,0 +1,31 @@
+import { Plugin, PluginOptionAPI, PluginFunction } from 'vuepress-types'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Mergeable = require('deconstruct-merge')
+
+export type MergeType = 'array' | 'assign' | 'flat' | 'override'
+
+export interface MergeOptions {
+  [key: string]: MergeType | MergeType[] | MergeOptions
+}
+
+function vuepressMergeable<T>(
+  plugin: Plugin<T>,
+  mergeOptions: MergeOptions,
+  defaultOptions: T
+): Plugin<T> {
+  let pluginFunction: PluginFunction
+  if (typeof plugin !== 'function') {
+    pluginFunction = (): PluginOptionAPI => plugin
+  } else {
+    pluginFunction = plugin
+  }
+
+  const config = new Mergeable(mergeOptions).merge(defaultOptions)
+
+  return (options, context): PluginOptionAPI => {
+    return pluginFunction(config.merge(options).value(), context)
+  }
+}
+
+module.exports = vuepressMergeable
